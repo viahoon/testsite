f7449884722c3ba29ebaef9c62a023576b6b1a16
viahoon <hoon@viasofts.com>
2020-10-18 15:25:33 +0900

    Updates

---
+++ precache-manifest.ec2fb52ab22c956630727eceb0903d94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80f8291af8d656635cb8287d280f54f",
+    "url": "/testsite/index.html"
+  },
+  {
+    "revision": "df1a61fcbbb52ab6159f",
+    "url": "/testsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5eee50dc9bb1e3ba0aa8",
+    "url": "/testsite/static/js/2.dd5a0804.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/testsite/static/js/2.dd5a0804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1a61fcbbb52ab6159f",
+    "url": "/testsite/static/js/main.b0db02bc.chunk.js"
+  },
+  {
+    "revision": "2f227ba62de6664d9901",
+    "url": "/testsite/static/js/runtime-main.26cdd4bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
